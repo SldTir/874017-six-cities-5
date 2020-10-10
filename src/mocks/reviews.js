30e4e50022764e29c305405d1efc196201b173f0
SldTir <54772323+SldTir@users.noreply.github.com>
2020-10-10 15:53:53 +0300

    Создал моки отзывов

---
+++ src/mocks/reviews.js
@@ -0,0 +1,90 @@
+export default [
+  {
+    id: 1,
+    reviews: [
+      {
+        avatar: `avatar-angelina.jpg`,
+        name: `Angelina`,
+        rating: 3.8,
+        date: new Date(),
+        reviewText: `Ladyship it daughter securing procured or am moreover mr. 
+        Put sir she exercise vicinity cheerful wondered. 
+        Continual say suspicion provision you neglected sir curiosity unwilling.`,
+      },
+      {
+        avatar: `avatar-max.jpg`,
+        name: `Max`,
+        rating: 3.1,
+        date: new Date(),
+        reviewText: `Simplicity end themselves increasing led day sympathize yet.
+        General windows effects not are drawing man garrets.`,
+      },
+    ]
+  },
+  {
+    id: 2,
+    reviews: [
+      {
+        avatar: `avatar-angelina.jpg`,
+        name: `Angelina`,
+        rating: 4.1,
+        date: new Date(),
+        reviewText: `Ladyship it daughter securing procured or am moreover mr. 
+        Put sir she exercise vicinity cheerful wondered. 
+        Continual say suspicion provision you neglected sir curiosity unwilling.`,
+      },
+      {
+        avatar: `avatar-max.jpg`,
+        name: `Max`,
+        rating: 3.7,
+        date: new Date(),
+        reviewText: `Simplicity end themselves increasing led day sympathize yet.
+        General windows effects not are drawing man garrets.`,
+      },
+      {
+        avatar: `avatar-angelina.jpg`,
+        name: `Angelina`,
+        rating: 2.2,
+        date: new Date(),
+        reviewText: `Ladyship it daughter securing procured or am moreover mr. 
+        Put sir she exercise vicinity cheerful wondered. 
+        Continual say suspicion provision you neglected sir curiosity unwilling.`,
+      },
+    ]
+  },
+  {
+    id: 3,
+    reviews: [
+      {
+        avatar: `avatar-angelina.jpg`,
+        name: `Angelina`,
+        rating: 1.1,
+        date: new Date(),
+        reviewText: `Ladyship it daughter securing procured or am moreover mr. 
+        Put sir she exercise vicinity cheerful wondered. 
+        Continual say suspicion provision you neglected sir curiosity unwilling.`,
+      },
+      {
+        avatar: `avatar-max.jpg`,
+        name: `Max`,
+        rating: 2.2,
+        date: new Date(),
+        reviewText: `Simplicity end themselves increasing led day sympathize yet.
+        General windows effects not are drawing man garrets.`,
+      }
+    ]
+  },
+  {
+    id: 4,
+    reviews: [
+      {
+        avatar: `avatar-max.jpg`,
+        name: `Max`,
+        rating: 4.9,
+        date: new Date(),
+        reviewText: `Simplicity end themselves increasing led day sympathize yet.
+        General windows effects not are drawing man garrets.`,
+      }
+    ]
+  }
+];
